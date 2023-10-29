047e3f1217afdd10d808174b6646b5f8132317d6
Héctor Orón Martínez <hector.oron@gmail.com>
2023-10-29 18:58:55 +0100

    ejercicio 1
    Signed-off-by: Héctor Orón Martínez <hector.oron@gmail.com>

---
+++ principiantes/ej1.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+# Escribir una secuencia de instrucciones de Python que permita declarar
+# 3 variables a, b, c asignándoles respectivamente los valores de 1,
+# 'Francia', 36.2 y mostrar valores por consola.
+
+a = 1
+b = 'Francia'
+c = 36.2
+
+print(a)
+print(b)
+print(c)
